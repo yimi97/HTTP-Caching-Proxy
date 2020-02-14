265105b8fbe8843b9e0c4597f967e387df3d1b2f
yx136 <yx136@duke.edu>
2020-02-14 18:16:17 -0500

    proxy receive browser request and print

--- docker-deploy/myProxy/main.cpp
+++ docker-deploy/myProxy/main.cpp
@@ -3,12 +3,22 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <vector>
+#include <cstring>
+
+
+#define PORT "8080"  // the port users will be connecting to
+
+#define BACKLOG 1000   // how many pending connections queue will hold
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    struct sockaddr_storage their_addr;
+    const char *hostname = NULL; // hostname set to NULL
+
+    struct sockaddr_storage their_addr; // connector's address information
     socklen_t addr_size;
     struct addrinfo hints, *res;
-    int socket_fd, new_fd;
+    int socket_fd, new_fd; // listen on sock_fd, new connection on new_fd
 
     // first, load up address structs with getaddrinfo():
 
@@ -17,7 +27,7 @@ int main() {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-    getaddrinfo(NULL, MYPORT, &hints, &res);
+    getaddrinfo(hostname, PORT, &hints, &res); // load up addrinfo res with getaddrinfo()
 
     // make a socket, bind it, and listen on it:
 
@@ -25,14 +35,23 @@ int main() {
     bind(socket_fd, res->ai_addr, res->ai_addrlen);
     listen(socket_fd, BACKLOG);
 
-    // now accept an incoming connection:
+    // now accept each incoming connection:
+    int thread_id = 1;
     while(1){
         addr_size = sizeof their_addr;
+        std::cout << "I am listening." << std::endl;
         new_fd = accept(socket_fd, (struct sockaddr *)&their_addr, &addr_size);
         if (new_fd == -1){
             exit(1);
         }
-        
+        // create a new thread for each client connection
+        std::vector<char> buffer(65536);
+        char* p = buffer.data();
+        int len = recv(new_fd, p, 65536, 0);
+        if (len < 0){
+            exit(1);
+        }
+        std::cout << p << std::endl;
     }
 
     close(socket_fd);
