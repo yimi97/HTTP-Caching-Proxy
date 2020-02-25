b79506dae95c8ffbae115dce505cc1c1cfb0d72f
ym154 <ym154@duke.edu>
2020-02-25 00:33:48 -0500

    parse header

--- docker-deploy/myProxy/response.h
+++ docker-deploy/myProxy/response.h
@@ -21,7 +21,7 @@ private:
     std::string status_text; // A brief, purely informational, textual description of the status code
                             // to help a human understand the HTTP message
 
-    std::map<std::string, std::string> headers;
+    std::map<std::string, std::string> header;
     std::string response_body;
 
 public:
@@ -32,10 +32,24 @@ public:
         protocol_vision = parse_protocol_vision();
         status_code = parse_status_code();
         status_text = parse_status_text();
+        header = parse_header();
 
     }
     Response(Response &rhs){}
     Response &operator=(Response &rhs){ return *this; }
+
+    std::map<std::string, std::string> parse_header() {
+        string content = response.substr(response.find("\r\n") + 2, response.find("\r\n\r\n"));
+        while(content.find("\r\n")!=string::npos) {
+            if(content.substr(content.find("\r\n"))=="\r\n") {break;}
+            std::string header_line = content.substr(0, content.find("\r\n"));
+            std::string key = header_line.substr(0, header_line.find(":"));
+            std::string value = header_line.substr(header_line.find(":") + 1);
+            header[key] = value;
+            content = content.substr(content.find("\r\n") + 2);
+        }
+        return header;
+    }
 
     std::string parse_status_line(){ return response.substr(0, response.find("\r\n")); }
     std::string parse_protocol_vision(){ return status_line.substr(0, status_line.find(" ")); }
@@ -50,6 +64,7 @@ public:
         return tmp;
     }
 
+    std::map<std::string, std::string> get_header(){ return header; }
     std::string get_status_line(){ return status_line; }
     std::string get_protocol_vision(){ return protocol_vision; }
     std::string get_status_code(){ return status_code; }
